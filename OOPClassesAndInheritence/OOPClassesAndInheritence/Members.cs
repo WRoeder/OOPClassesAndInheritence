844336c39cf7be72155ad4b4a031a90a86a8f12e
WRoeder <77704216+WRoeder@users.noreply.github.com>
2021-02-10 13:55:48 -0500

    First Check In

---
+++ OOPClassesAndInheritence/OOPClassesAndInheritence/Members.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassesAndInheritence
+{
+    class Members
+    {
+        public string MemberName;
+
+        public Members(string _name = null)
+        {
+            MemberName = _name;
+
+            Console.WriteLine("Member " + _name + " created at " + DateTime.Now.ToString("yyyy-MM-dd"));
+        }
+    }
+}
---
+++ OOPClassesAndInheritence/OOPClassesAndInheritence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassesAndInheritence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Linkin Park Library
+
+            Console.WriteLine("The members of Linkin Park are: ");
+            Members ChesterBennington = new Members("Chester Bennington");
+            Members MikeShinoda = new Members("Mike Shinoda");
+            Members JoeHahn = new Members("Joe Harn");
+            Members RobBourden = new Members("Rob Bourden");
+            Members BradDelson = new Members("Brad Delson");
+            Members DaveFarrell = new Members("Dave Farrell");
+
+            Console.WriteLine("What is the name of your member?: ");
+            string newMember = Console.ReadLine();
+
+            Console.WriteLine("What album do you want to listen to?: ");
+            string newAlbum = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
